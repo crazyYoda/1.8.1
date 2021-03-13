938ce9209114347a071cffe761fdfeca9574dc7b
CrazyYoda <udzumaki79@gmail.com>
2021-03-13 13:48:05 +0300

    First edit

---
+++ Cats_available.py
@@ -0,0 +1,26 @@
+from main import Cat
+
+cats = [
+{
+     "name": "Феликс",
+     "gender": "Мальчик",
+     "age": "2",
+    },
+{
+     "name": "Сэм",
+     "gender": "Мальчик",
+     "age": "2",
+    },
+]
+
+print('Коты в наличии')
+print('^^^^^^^^^^^^^^^')
+
+for pet in cats:
+    unit = Cat(name=pet.get("name"), gender=pet.get("gender"), age=pet.get("age"))
+    print(unit.name)
+    print(unit.gender)
+    print(unit.age)
+    print()
+    
+print('^^^^^^^^^^^^^^^')
---
+++ main.py
@@ -0,0 +1,14 @@
+class Cat:
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
+
+    def get_name(self):
+        return self.width
+
+    def get_gender(self):
+        return self.height
+
+    def get_age(self):
+        return self.age
